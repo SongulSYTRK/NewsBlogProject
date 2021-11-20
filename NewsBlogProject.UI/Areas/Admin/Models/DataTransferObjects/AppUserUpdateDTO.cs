e468bdd51f25126c3a6c711755aa18a6e0150be7
songul soyturk <songulsytrk@gmail.com>
2021-11-20 16:53:44 +0300

    Automapper added

--- NewsBlogProject.UI/Areas/Admin/Models/DataTransferObjects/AppUserUpdateDTO.cs
+++ NewsBlogProject.UI/Areas/Admin/Models/DataTransferObjects/AppUserUpdateDTO.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using NewsBlogProject.Model.Enums;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -32,5 +33,23 @@ namespace NewsBlogProject.UI.Areas.Admin.Models.DataTransferObjects
 
         [NotMapped]
         public IFormFile ImagePath { get; set; }
+
+
+
+        public DateTime? CreateDate { get; set; }
+        public string CreateComputerName { get; set; }
+        public string CreateIp { get; set; }
+
+
+        public DateTime? DeleteDate { get; set; }
+        public string DeleteComputerName { get; set; }
+        public string DeleteIp { get; set; }
+
+
+        public DateTime? UpdateDate { get; set; }
+        public string UpdateComputerName { get; set; }
+        public string UpdateIp { get; set; }
+
+        public Status Status { get; set; }
     }
 }
--- NewsBlogProject.UI/Areas/Admin/Models/DataTransferObjects/CategoryUpdateDTO.cs
+++ NewsBlogProject.UI/Areas/Admin/Models/DataTransferObjects/CategoryUpdateDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NewsBlogProject.Model.Enums;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -17,5 +18,21 @@ namespace NewsBlogProject.UI.Areas.Admin.Models.DataTransferObjects
         [Required(ErrorMessage = "must to type into name")]
         [MinLength(3, ErrorMessage = "Min length is 3")]
         public string Description { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+        public string CreateComputerName { get; set; }
+        public string CreateIp { get; set; }
+
+
+        public DateTime? DeleteDate { get; set; }
+        public string DeleteComputerName { get; set; }
+        public string DeleteIp { get; set; }
+
+
+        public DateTime? UpdateDate { get; set; }
+        public string UpdateComputerName { get; set; }
+        public string UpdateIp { get; set; }
+
+        public Status Status { get; set; }
     }
 }
